e7fe944e92fae6c421e6137dd77e329a0f6cda43
Arctic403 <kc.campbell132@gmail.com>
2026-09-02 02:20:19 -0400

    Android: use stable API 36 SDK baseline

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.riftos.app"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.riftos.app"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1.0-android-alpha"
     }
